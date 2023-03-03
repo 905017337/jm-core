e0d42381305081b73ef88610f2640d83fd67bdfe
caozhenhao <905017337@qq.com>
2023-03-03 20:47:58 +0800

    初始化代码

---
+++ src/main/java/com/czh/jm/JmCoreApplication.java
@@ -0,0 +1,13 @@
+package com.czh.jm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/czh/jm/JmCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czh.jm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
